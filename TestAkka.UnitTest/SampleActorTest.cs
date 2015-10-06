14af87e656bcb5d3938e0ea96a34ec0129a1efe9
Alberto Monteiro <alberto.monteiro@live.com>
2015-10-06 15:54:55 -0300

    Learning unit teste with Akka.TestKit

---
+++ TestAkka.UnitTest/SampleActorTest.cs
@@ -0,0 +1,27 @@
+﻿using Akka.Actor;
+using Akka.TestKit.NUnit;
+using NUnit.Framework;
+
+namespace TestAkka.UnitTest
+{
+    [TestFixture]
+    public class SampleActorTest : TestKit
+    {
+        public class MyFirstActor : ReceiveActor
+        {
+            public MyFirstActor()
+            {
+                Receive<string>(s => Sender.Tell(s));
+            }
+        }
+
+        [Test]
+        public void MyFirstActorShouldPrintOnConsole()
+        {
+            var actorRef = Sys.ActorOf(Props.Create<MyFirstActor>());
+
+            actorRef.Tell("Ola");
+            ExpectMsg("Ola");
+        }
+    }
+}
